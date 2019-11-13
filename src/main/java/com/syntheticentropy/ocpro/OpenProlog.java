9836fa6a0665b8e905cbc3491c659c34b9fd6e8f
Zachery Delafosse <natsogames@yahoo.com>
2019-11-13 14:54:59 -0600

    Updating Mod ID and other versioning points

--- src/main/java/com/syntheticentropy/ocpro/OpenProlog.java
+++ src/main/java/com/syntheticentropy/ocpro/OpenProlog.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.syntheticentropy.ocpro;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
@@ -7,11 +7,11 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = OpenProlog.MODID, name = OpenProlog.NAME, version = OpenProlog.VERSION)
+public class OpenProlog
 {
-    public static final String MODID = "examplemod";
-    public static final String NAME = "Example Mod";
+    public static final String MODID = "ocpro";
+    public static final String NAME = "OpenProlog";
     public static final String VERSION = "1.0";
 
     private static Logger logger;
