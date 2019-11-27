76d33d7d2a45522bffc71403a9332bf0574d2620
Zachery Delafosse <natsogames@yahoo.com>
2019-11-27 10:42:54 -0600

    Removed several jiprolog libraries. xcall is still available so don't consider this safe, just tidier.

--- src/main/resources/com/ugos/jiprolog/x.pl
+++ src/main/resources/com/ugos/jiprolog/x.pl
@@ -8,11 +8,11 @@ jip_init_modules :-
 	use_module('INTERNAL://com/ugos/jiprolog/sys.pl'),
 	use_module('INTERNAL://com/ugos/jiprolog/xsets.pl'),
 	use_module('INTERNAL://com/ugos/jiprolog/setof.pl'),
-	use_module('INTERNAL://com/ugos/jiprolog/xio.pl'),
-	use_module('INTERNAL://com/ugos/jiprolog/xdb.pl'),
+	%use_module('INTERNAL://com/ugos/jiprolog/xio.pl'),
+	%use_module('INTERNAL://com/ugos/jiprolog/xdb.pl'),
 	%use_module('INTERNAL://com/ugos/jiprolog/xexception.pl'),
-	use_module('INTERNAL://com/ugos/jiprolog/xreflect.pl'),
-	use_module('INTERNAL://com/ugos/jiprolog/xsystem.pl'),
+	%use_module('INTERNAL://com/ugos/jiprolog/xreflect.pl'),
+	%use_module('INTERNAL://com/ugos/jiprolog/xsystem.pl'),
 	%use_module('INTERNAL://com/ugos/jiprolog/xxml.pl'),
 	use_module('INTERNAL://com/ugos/jiprolog/xterm.pl').
 
@@ -24,11 +24,11 @@ jip_init_modules :-
 	use_module('INTERNAL://com/ugos/jiprolog/sys.jip'), %write('sys.jip'), nl,
 	use_module('INTERNAL://com/ugos/jiprolog/xsets.jip'), %write('xsets.jip'), nl,
 	use_module('INTERNAL://com/ugos/jiprolog/setof.jip'), %write('xsets.jip'), nl,
-	use_module('INTERNAL://com/ugos/jiprolog/xio.jip'), %write('xio.jip'), nl,
-	use_module('INTERNAL://com/ugos/jiprolog/xdb.jip'), %write('xdb.jip'), nl,
+	%use_module('INTERNAL://com/ugos/jiprolog/xio.jip'), %write('xio.jip'), nl,
+	%use_module('INTERNAL://com/ugos/jiprolog/xdb.jip'), %write('xdb.jip'), nl,
 	%use_module('INTERNAL://com/ugos/jiprolog/xexception.jip'), %write('xexception.jip'), nl,
-	use_module('INTERNAL://com/ugos/jiprolog/xreflect.jip'), %write('xreflect.jip'), nl,
-	use_module('INTERNAL://com/ugos/jiprolog/xsystem.jip'), %write('xsystems.jip'), nl,
+	%use_module('INTERNAL://com/ugos/jiprolog/xreflect.jip'), %write('xreflect.jip'), nl,
+	%use_module('INTERNAL://com/ugos/jiprolog/xsystem.jip'), %write('xsystems.jip'), nl,
 	%use_module('INTERNAL://com/ugos/jiprolog/xxml.jip'), %write('xxml.jip'), nl.
 	use_module('INTERNAL://com/ugos/jiprolog/xterm.jip'). %write('xterm.jip'), nl.
 
